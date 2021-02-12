839a1bfa0b3ba5c143290fd577a5ec9fc02a0d9e
jonmenard007 <55707155+jonmenard007@users.noreply.github.com>
2021-02-12 11:04:32 -0500

    fixing build

--- src/main/java/com/example/testingdatajpa/TestingWebApplicationTests.java
+++ src/main/java/com/example/testingdatajpa/TestingWebApplicationTests.java
@@ -8,7 +8,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.example.accessingdatajpa.FrontEndController;
-import com.example.accessingdatajpa.TestController;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
